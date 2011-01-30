179b23069da05e316f37f9c0d195dd7d451dc2b4
Kevin Charter <kevin.charter@acm.org>
2011-01-30 15:50:51 -0700

    Rename the internal constraint types.
    The internal constraint types are in module BRC.Solver.ZeroOneTwo,
    but the internal constraint types are called
    
     NoVars
     OneVar
     TwoVars
    
    I've changed 'No' to 'Zero' and dropped the trailing 's', so now
    the types are called
    
     ZeroVar
     OneVar
     TwoVar
    
    Everything still compiles and the tests all run.

--- BRC/Solver/ZeroOneTwo.hs
+++ BRC/Solver/ZeroOneTwo.hs
@@ -7,17 +7,17 @@ import Data.List (foldl')
 import BRC.Constraint
        
 -- | An internal constraint with no variables, just constants.
-data NoVars v e = NoVars e e;
+data ZeroVar v e = ZeroVar e e;
 
 -- | An internal constraint with exactly one variable and one constant.
 data OneVar v e = OnLeft v e | OnRight e v;
 
 -- | An internal constraint with exactly two variables
-data TwoVars v e = TwoVars v v;
+data TwoVar v e = TwoVar v v;
 
 -- | Transforms a list of constraints into lists of zero-variable,
 -- one-variable, and two-variable constraints.
-toZeroOneTwo :: [Constraint v e] -> ([NoVars v e], [OneVar v e], [TwoVars v e])
+toZeroOneTwo :: [Constraint v e] -> ([ZeroVar v e], [OneVar v e], [TwoVar v e])
 toZeroOneTwo =
   reverse3 . foldl' splitOne ([],[],[])
   where splitOne (zeros, ones, twos) c =
@@ -25,7 +25,7 @@ toZeroOneTwo =
               Variable v ->
                 case right c of
                   Variable w ->
-                    (zeros, ones, (TwoVars v w):twos)
+                    (zeros, ones, (TwoVar v w):twos)
                   Constant x ->
                     (zeros, (OnLeft v x):ones, twos)
               Constant x ->
@@ -33,10 +33,10 @@ toZeroOneTwo =
                   Variable w ->
                     (zeros, (OnRight x w):ones, twos)
                   Constant y ->
-                    ((NoVars x y):zeros, ones, twos)
+                    ((ZeroVar x y):zeros, ones, twos)
         reverse3 (xs,ys,zs) = (reverse xs, reverse ys, reverse zs)
 
-fromZeroOneTwo :: ([NoVars v e], [OneVar v e], [TwoVars v e]) -> [Constraint v e]
+fromZeroOneTwo :: ([ZeroVar v e], [OneVar v e], [TwoVar v e]) -> [Constraint v e]
 fromZeroOneTwo (zeros,ones,twos) =
   toConstraints zeros ++ toConstraints ones ++ toConstraints twos
 
@@ -46,12 +46,12 @@ toConstraints = fmap toConstraint
 class ToConstraint v e p | p -> e, p -> v where
   toConstraint :: p -> Constraint v e
   
-instance ToConstraint v e (NoVars v e) where
-  toConstraint (NoVars x y) = Related (Constant x) (Constant y)
+instance ToConstraint v e (ZeroVar v e) where
+  toConstraint (ZeroVar x y) = Related (Constant x) (Constant y)
   
 instance ToConstraint v e (OneVar v e) where
   toConstraint (OnLeft v x) = Related (Variable v) (Constant x)
   toConstraint (OnRight x v) = Related (Constant x) (Variable v)
 
-instance ToConstraint v e (TwoVars v e) where
-  toConstraint (TwoVars v w) = Related (Variable v) (Variable w)
+instance ToConstraint v e (TwoVar v e) where
+  toConstraint (TwoVar v w) = Related (Variable v) (Variable w)
