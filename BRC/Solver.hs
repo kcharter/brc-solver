32f0b241cb995d345d08d8854ca06ae768b9bb82
Kevin Charter <kevin.charter@acm.org>
2011-01-30 10:22:17 -0700

    Add a stub of the BRC.Solver module.
    The solver is just a stub now, but I've added the types for
    the interface.

---
+++ BRC/Solver.hs
@@ -0,0 +1,24 @@
+module BRC.Solver (Relatee(..), Constraint(..), Binding(..), solve) where
+
+import BRC.BinRel
+import BRC.SetOf
+
+data Relatee v e =
+  Variable v
+  | Constant e
+    deriving (Eq, Ord, Show)
+             
+data Constraint v e = Related { left, right :: Relatee v e } deriving (Eq, Ord, Show)
+
+data Binding v e = Binding { variable :: v, value :: e } deriving (Eq, Ord, Show)
+
+solve :: (Ord v, SetOf e s) => BinRel e s -> [Constraint v e] -> [[Binding v e]]
+solve = error "not implemented"
+
+-- internal state/error monad for the solver (so we can return an error message)
+-- split constraints into three simpler constraint types: zero-variable, one-variable, two-variable
+-- solve ins three passes:
+-- 1. make sure there are no contradictions in zero-variable constraints
+-- 2. build up a mapping of variables to sets using the single-variable constraints
+-- 3. lazily enumerate Cartesian product, filtered using two-variable constraints
+--    - stack to allow back-tracking?
