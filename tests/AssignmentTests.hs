fbcfb40b0b02ebd9c92fdd93f193aed507e40845
Kevin Charter <kevin.charter@acm.org>
2011-02-05 21:39:31 -0700

    First unit test for BRC.Solver.Assignments.
    It took a while to build up the code for this, but I've
    got a test for finding unique solutions, and some supporting
    stuff for some other tests where the solutions need not
    be unique.
    
    This test takes a generated assignment on bounded, enumerable,
    ordered domain, and then builds a set of less-than constraints
    that have a unique solution. If X is assigned V, then
    
    - when V is the domain minimum, we add constraint
    
      X < succ V
    
    - when V is the domain maximum, we add constraint
    
      pred V < X
    
    - and otherwise, we add the two constraints
    
      pred V < X   and
      X < succ V
    
    This uniquely determines X, and we can convert the constraints
    into predicates acceptable to 'subjectTo'. Since the constraints
    have a unique solution,
    
    1. 'subjectTo' should find exactly one assignment, provided
       the maxTrialsPerVariable is set to the size of the domain
    
    2. the one assignment must be the same as the original
       assignment from which we derived the constraints
    
    While doing this, I found a bug in the 'nextVar' function
    in 'subjectTo' (addressed in the last commit), and I also
    found a shortcoming in the interface of that function:
    it silently discards all but one predicate on the same
    pair of variables. For example, suppose the domain is
    {1,2,3,4,5}, and X is assigned 3 in the test assignment.
    In the constraints we derive from the assignment, we have
    both
    
      2 < X    and    X < 4
    
    This translates into the 'subjectTo' predicates
    
     ((X,X), const (2<))  and   ((X,X), const (<4))
    
    But 'subjectTo' assumes that each predicate has
    a unique pair of variables in the first component.
    So if you give 'subjectTo' the predicate list
    
     [((X,X), const (2<)), ((X,X), const (<4))]
    
    it will silently discard one of the predicates. It's up
    to the client code to conjoin the predicates into one,
    like so
    
     [((X,X), (\x y -> const (2<) x y && const (<4) x y)]
    
    This is a definite interface wart, because it would be
    easy for 'subjectTo' to conjoin the predicates itself.
    
    I'll likely fix that problem after I add the next test.

--- tests/AssignmentTests.hs
+++ tests/AssignmentTests.hs
@@ -1,7 +1,156 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module AssignmentTests where
 
+import Control.Arrow (second)
+import Control.Monad (liftM, liftM2)
+import Data.Ord (comparing)
+import Data.List (foldl', nub, sortBy)
+import qualified Data.Map as DM
+import qualified Data.Set as DS
+import Data.Maybe (mapMaybe, catMaybes)
 import Test.QuickCheck
 
-runAll :: (Bounded a, Ord a, Show a) => Gen a -> IO ()
-runAll g = return ()
+import BRC.Solver.Assignments (subjectTo, defaultSearchOptions, maxTrialsPerVariable)
+
+import Variable
+
+runAll :: (Bounded a, Enum a, Ord a, Show a) => Gen a -> IO ()
+runAll g = mapM_ quickCheck [
+  forAll (assignment g) prop_findsUniqueSolutions
+  ]
+
+prop_findsUniqueSolutions :: (Bounded a, Enum a, Ord a, Show a) =>
+                             [(Variable, a)] -> Bool
+prop_findsUniqueSolutions assignment =
+  let assignment' = DM.toList $  DM.fromList $ assignment
+      oneVarConstraints = oneVarLessThans assignment'
+      solutions = allSolutions oneVarConstraints
+  in length solutions == 1 && assignment' == head solutions
+  
+-- | For a value generator, generates a pairing of variables with
+-- values. Each variable occurs at most once.
+assignment :: Gen a -> Gen [(Variable, a)]
+assignment val = (DM.toList . DM.fromList) `liftM` listOf binding
+                 where binding = liftM2 (,) arbitrary val 
+
+-- | Computes all solutions of a set of contraints, ignoring
+-- constraints that contain no variables. The variable assignments are
+-- sorted by variable.
+allSolutions :: forall e .(Bounded e, Enum e, Ord e) => [LTC e] -> [[(Variable, e)]]
+allSolutions constraints =
+  let predicates = toPredicates constraints
+      domainSize = intervalWidth domainMin domainMax
+      possibles  = zip allVars (repeat universe)
+      allVars    = DS.toList $ DS.fromList $ concatMap variables constraints
+      universe   = [domainMin .. domainMax]
+      domainMin  = minBound :: e
+      domainMax  = maxBound :: e
+      opts       =
+        defaultSearchOptions {
+          maxTrialsPerVariable = domainSize }
+  in map (sortBy (comparing fst)) (subjectTo opts predicates possibles)
+     
+-- | Computes the size of the closed interval bounded by two domain
+-- values. This is useful for computing the size of a finite domain.
+intervalWidth :: (Bounded e, Enum e) => e -> e -> Int
+intervalWidth x y = abs (fromEnum y - fromEnum x) + 1
+
+-- | Combines all the predicates represented by list of
+-- constraints. When more than one predicate appears for the same pair
+-- of variables, they are conjoined into a single predicate. All pairs
+-- of variables are unique in the result.
+toPredicates :: (Ord e) => [LTC e] -> [((Variable, Variable), e -> e -> Bool)]
+toPredicates =
+  map (second conjoinAll) . accumPredicates . mapMaybe asPredicate 
+  where accumPredicates = byFirst
+        conjoinAll = foldl' conjoin (const (const True))
+        conjoin pred1 pred2 x y = pred1 x y && pred2 x y
+        
+-- | Converts a less-than constraint into the form of predicate
+-- accepted by 'subjectTo'.
+asPredicate :: (Ord e) => LTC e -> Maybe ((Variable, Variable), e -> e -> Bool)
+asPredicate (LTC a b) =
+  -- I find the case analysis a bit easier to read than calling 'either'
+  case a of
+    Left varA ->
+      case b of
+        Left varB  -> Just ((varA, varB), (<))
+        Right valB -> Just ((varA, varA), const (< valB))
+    Right valA ->
+      case b of
+        Left varB  -> Just ((varB, varB), const (valA <))
+        Right _    -> Nothing
+
+-- | Given an assignment, builds a list of one-variable less-than
+-- constraints that are uniquely solved by the assignment. Each
+-- variable is pinned between the (at most) two constants on either
+-- side of it.
+oneVarLessThans :: (Bounded e, Enum e, Ord e) => [(Variable, e)] -> [LTC e]
+oneVarLessThans bindings =
+  concatMap oneVarLts (varsByValue bindings)
+  where oneVarLts (val, vars) =
+          maybe [] (\x -> [lt (valArg x) (varArg y) | y <- vars]) (before val) ++
+          maybe [] (\x -> [lt (varArg y) (valArg x) | y <- vars]) (after val)
+        before x = if x == minBound then Nothing else Just (pred x)
+        after x  = if x == maxBound then Nothing else Just (succ x)
+
+-- | Given an assignment, builds a list of less-than constraints
+-- between variables that are assigned adjacent values. For example,
+-- if @X@ is assigned @1@ and @Y@ is assigned @14@, and no variable is
+-- assigned a value between @2@ and @13@, then the constraints include
+-- @X < Y@. The resulting set of constraints is satisfied by the
+-- original assignment, but need not be the only possible solution.
+twoVarLessThans :: (Bounded e, Enum e, Ord e) => [(Variable, e)] -> [LTC e]
+twoVarLessThans bindings =
+  twoVarLts (groupEqualVars bindings)
+    where twoVarLts = concat . fst . foldl' accumTwoVarLts ([],[])
+          accumTwoVarLts (sofar, prevVars) vars = (lts:sofar, vars)
+            where lts = [lt (varArg x) (varArg y) |
+                         x <- prevVars, y <- vars]
+
+-- | For a list of bindings, the lists of variables assigned to each
+-- value.
+varsByValue :: (Ord a) => [(Variable, a)] -> [(a, [Variable])]
+varsByValue = bySecond
+
+-- | Given a list of pairs, returns a list pairing unique first values
+-- with lists of corresponding second values.
+byFirst :: (Ord f) => [(f, s)] -> [(f, [s])]
+byFirst = DM.toList . foldl' accumByFirst DM.empty
+  where accumByFirst m (first,second) =
+          DM.alter (Just . accum second) first m
+
+-- | Given a list of pairs, returns a list pairing unique second values
+-- with lists of corresponding first values.
+bySecond :: (Ord s) => [(f, s)] -> [(s, [f])]
+bySecond = DM.toList . foldl' accumBySecond DM.empty
+  where accumBySecond m (first,second) =
+          DM.alter (Just . accum first) second m
+                
+-- | Either starts a list where none existed before, or prepends to an
+-- existing one. Intended for use with 'Data.Map.alter'.
+accum :: a -> Maybe [a] -> [a]
+accum what Nothing = [what]
+accum what (Just sofar) = what:sofar
+                
+-- | For a list of bindings, the lists of equal variables, in
+-- ascending order of the value to which they are bound.
+groupEqualVars :: (Ord a) => [(Variable, a)] -> [[Variable]]
+groupEqualVars = map snd . varsByValue
+
+-- | The unique variables appearing in a constraint.
+variables :: LTC e -> [Variable]
+variables (LTC a b) =
+  nub $ catMaybes [argVariables a, argVariables b]
+  where argVariables = either Just (const Nothing)
+        
+lt = LTC
+
+data LTC e = LTC (Arg e) (Arg e) deriving (Eq, Ord, Show)
+
+type Arg e = Either Variable e
+
+varArg = Left
+valArg = Right
 
